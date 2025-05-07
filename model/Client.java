5a253fe4e48d51ecfae50e93124b7c6db42e83d0
smeg24 <smeg24@students.cs.ubc.ca>
2025-05-07 00:55:53 -0700

    created client chat (incomplete)

--- model/Client.java
+++ model/Client.java
@@ -9,6 +9,7 @@ public class Client extends User {
     private Socket s;    
     private BufferedReader in; 
     private PrintWriter out; 
+    private BufferedReader inputC; 
 
     // bulding connection like wire b/w client and server 
     public Client(String n, String ip, int port) { 
@@ -17,9 +18,12 @@ public class Client extends User {
             // connect to server on port no. 
             s = new Socket(ip, port);     
             System.out.println("Connected!");
+
+            // read client message
+            inputC = new BufferedReader(new InputStreamReader(System.in)); 
              
-            // Take input from terminal. s.getInputStream like a pipe and open mailbox and 
-            // return raw data (binary) 
+            // Take input from terminal (server's messsage only). s.getInputStream like a pipe and 
+            // open mailbox and return raw data (binary) 
             // inputStreamerReader is like a trasnlator; decode bytes to meaningful string
             in = new BufferedReader(new InputStreamReader(s.getInputStream())); 
             // connect output stream to the socket. think of it like wire 
@@ -27,12 +31,20 @@ public class Client extends User {
             // a robot 
             out = new PrintWriter(s.getOutputStream(), true); 
 
+            String line = " "; 
+            while (!line.equals("bye")) { 
+                line = inputC.readLine(); 
+                out.println(line);
+                System.out.println(in.readLine());
+            }
+
+            s.close(); // clear resources. help server know client is disconnected
+            inputC.close(); 
+            out.close(); 
         } catch(UnknownHostException e) {  
-            System.out.println(e);
-            return; 
+            System.out.println("Host Unknown: " + e.getMessage());
         } catch (IOException e) { 
-            System.out.println(e);
-            return; 
+            System.out.println("Unexpected exception: " + e.getMessage());
         } 
 
 
